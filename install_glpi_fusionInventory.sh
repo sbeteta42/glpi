f4905ca71f8b9ce05951b14b7a135b382f0db33a
sbeteta42 <137415087+sbeteta42@users.noreply.github.com>
2025-08-30 12:56:53 +0200

    Update install_glpi_fusionInventory.sh

--- install_glpi_fusionInventory.sh
+++ install_glpi_fusionInventory.sh
@@ -1,10 +1,12 @@
 #/sbin/bash
-#########################################################
-# script d'installation auomatisé par sbeteta@beteta.org#
-#########################################################
+##################################################
+# script d'installation automatisé de GLPI 9.5.7 #
+# par sbeteta@beteta.org                         #
+##################################################
 
 # installation des dependances fonctionnelles
 echo "installation des dependances fonctionnelles"
+sleep 3
 apt -y install dmidecode hwdata ucf hdparmapt -y install dmidecode hwdata ucf hdparmapt -y install dmidecode hwdata ucf hdparm
 apt -y install dmidecode hwdata ucf hdparm
 apt -y install perl libuniversal-require-perl libwww-perl libparse-edid-perl
@@ -23,22 +25,25 @@ clear
 
 # on relance apache et mariadb
 echo "on active le service apache et mariadb"
+sleep 3
 /etc/init.d/apache2 restart
 /etc/init.d/mysql restart
 
 # creation de la base de données
 echo "creation de la base de données"
+sleep 2
 mysql -u root -e "create database glpidb;"
 mysql -u root -e "grant all privileges on glpidb.* to 'glpiuser'@'localhost' identified by 'operations';"
 mysql -u root -e "flush privileges;"
 
 # download de glpi
 echo " Download de GLPI 9.5.7"
+sleep 3
 wget https://github.com/glpi-project/glpi/releases/download/9.5.7/glpi-9.5.7.tgz
 clear
 
 #dezip de glpi dans /var/www/html
-echo "Decompression de l'archuve tarGZ vers /var/www/html"
+echo "Decompression de l'archive tarGZ vers /var/www/html"
 tar zxvf glpi-9.5.7.tgz -C /var/www/html
 clear
 
@@ -47,9 +52,11 @@ chown -R www-data /var/www/html/glpi
 
 # download de fusioninventory
 echo "Download de fusioninventory"
+sleep 3
 wget https://github.com/fusioninventory/fusioninventory-for-glpi/releases/download/glpi9.5%2B3.0/fusioninventory-9.5+3.0.tar.bz2
 
 # on detare fusioninventory dans /var/www/html/glpi/plugins
+sleep 2
 echo on detare fusioninventory dans /var/www/html/glpi/plugins
 tar jxvf fusioninventory-9.5+3.0.tar.bz2 -C /var/www/html/glpi/plugins
 echo "Installtion de GLPI et FUSIONINVENTORY terminé" 
